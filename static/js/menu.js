f616518e160506d6f51cb2f12a994791179a8e9f
deantosh daiddoh <deantoshdaiddoh@gmail.com>
2025-05-22 07:07:50 -0400

    Added: function that adds an active class to a nav link and function that closes the menu when user clicks outside of the menu section

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,8 +1,30 @@
-// This script toggles the visibility of a menu when a menu icon is clicked.
+// This function toggles the visibility of a menu when a menu icon is clicked.
 document.addEventListener('click', function(e) {
     const target = e.target;
     if (target.id === 'menu-icon') {
         const menu = document.getElementById('dropdown-menu');
         menu.classList.toggle('hidden');
+    }
+});
+
+// This function adds an active class to the menu link clicked.
+document.querySelectorAll('.menu-link').forEach(link => {
+    link.addEventListener('click', function(e) {
+        // Remove 'active' class from all links
+        document.querySelectorAll('.menu-link').forEach(l => l.classList.remove('active'));
+        // Add 'active' class to the clicked link
+        e.target.classList.add('active');
+        // Hide the menu after clicking a link
+        const menu = document.getElementById('dropdown-menu');
+        menu.classList.add('hidden');
+    })
+});
+
+// This function closes the menu when clicking outside of it.
+document.addEventListener('click', function(e) {
+    const menu = document.getElementById('dropdown-menu');
+    const menuIcon = document.getElementById('menu-icon');
+    if (!menu.contains(e.target) && !menuIcon.contains(e.target)) {
+        menu.classList.add('hidden');
     }
 });
